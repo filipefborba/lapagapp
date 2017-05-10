3f941dd2c779902ef5ef8c0e190adab119b6614c
gabsmoreira <gabriel.moreira98@hotmail.com>
2017-05-10 13:41:24 -0300

    fonts added

--- Lapag/app/src/main/java/com/example/gmore/lapag/initial_userActivity.java
+++ Lapag/app/src/main/java/com/example/gmore/lapag/initial_userActivity.java
@@ -225,10 +225,6 @@ public class initial_userActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
