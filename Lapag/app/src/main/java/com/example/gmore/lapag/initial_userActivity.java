1d04301e29edc6aacefa0c819b92a8556288d697
Filipe Foschini Borba <filipefborba@gmail.com>
2017-05-15 17:25:42 -0300

    Removed useless stuff and added Title to activity

--- Lapag/app/src/main/java/com/example/gmore/lapag/initial_userActivity.java
+++ Lapag/app/src/main/java/com/example/gmore/lapag/initial_userActivity.java
@@ -210,31 +210,6 @@ public class initial_userActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        //initial.setEnabled(true);
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.initial_user, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
