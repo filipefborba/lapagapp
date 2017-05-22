e1035b7456f0ae5ec7cf18026e37120d9d910ab6
Filipe Foschini Borba <filipefborba@gmail.com>
2017-05-22 18:01:37 -0300

    Added profile activity

--- Lapag/app/src/main/java/com/example/gmore/lapag/calendarActivity.java
+++ Lapag/app/src/main/java/com/example/gmore/lapag/calendarActivity.java
@@ -1,37 +1,25 @@
 package com.example.gmore.lapag;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Locale;
 
-import com.github.sundeepk.compactcalendarview.CompactCalendarView;
-import com.github.sundeepk.compactcalendarview.domain.Event;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 import java.util.TimeZone;
 
 public class calendarActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
@@ -138,8 +126,9 @@ public class calendarActivity extends AppCompatActivity implements NavigationVie
         } else if (id == R.id.nav_cale) {
             Intent intent = new Intent(this, calendarActivity.class);
             this.startActivity (intent);
-        } else if (id == R.id.nave_fale) {
-
+        } else if (id == R.id.nav_perfil) {
+            Intent intent = new Intent(this, profileActivity.class);
+            this.startActivity (intent);
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_calendar);
         drawer.closeDrawer(GravityCompat.START);
