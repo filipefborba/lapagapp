1231f1ba654ac28413062187e2c651a33d73359b
filipefborba <filipefborba@gmail.com>
2017-06-01 20:03:06 -0300

    Manual push notification working like a charm!

--- Lapag/app/src/main/java/com/example/gmore/lapag/MyFirebaseMessagingService.java
+++ Lapag/app/src/main/java/com/example/gmore/lapag/MyFirebaseMessagingService.java
@@ -106,7 +106,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.lapag2)
+                .setSmallIcon(R.drawable.ic_money)
                 .setContentTitle("FCM Message")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
